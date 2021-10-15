ffce375ca836c190f41b3bdb1b3d813348917145
3430 <3430@example.com>
2021-10-15 11:08:37 +0900

    ログイン画面

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,6 +9,15 @@ class HomeController < ApplicationController
   end
 
   def login_check
+    @user = User.find_by(user_id: "aaa", password: "aaaaa")
+    if  @user.present?
+      session[:login_id] = user_id
+      session[:login_user] = user_acount
+      redirect_to home_top_path
+    else
+      flash[:error] = "アカウントまたはパスワードが正しくありません"
+      render :login, layout: nil
+    end
   end
 
   def logout
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   get 'home/top'
   get 'home/kanri'
   get 'home/login'
   post 'home/login_check'
-=======
-
   post '/books_search', to: 'home#search'
   post '/', to: 'home#search'
->>>>>>> c033f3c248784f7846b89b200de595360e2882fa
   resources :books
   resources :users
   resources :questions
