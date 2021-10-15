f92b5f1bcef4b35f62e1c1f0a1644478018354ae
3430 <3430@example.com>
2021-10-15 12:49:57 +0900

    ログイン画面

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,20 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   get 'home/top'
   get 'home/kanri'
   get 'home/login'
   post 'home/login_check'
   post '/books_search', to: 'home#search'
-=======
   get 'home/kanri'
   get 'home/login'
   post 'home/login_check'
 
   post '/books_search', to:"home#search"
->>>>>>> 52c4a21accc205804d2d5f68a8ed18ee23a949bd
   post '/', to: 'home#search'
   resources :books
   resources :users
   resources :book_reviews
   resources :points
   resources :question_reviews
+  resources :questions
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
