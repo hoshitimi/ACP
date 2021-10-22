337ba995a8b7b318ae33e56aa3da98fccbac4489
3430 <3430@example.com>
2021-10-22 15:59:18 +0900

    現在のポイント数の表示

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -14,8 +14,9 @@ class HomeController < ApplicationController
     @user = User.find_by(user_id: @user_id, password: @password)
     if  @user.present?
       session[:login_id] = @user.user_id
-      session[:login_user] = @user.user_acount
-      session[:authority] = @user.authority      
+      session[:login_user_acount] = @user.user_acount
+      session[:authority] = @user.authority    
+      session[:login_user_point] = @user.point 
       redirect_to home_top_path
     else
       flash.now[:error] = "アカウントまたはパスワードが正しくありません"
