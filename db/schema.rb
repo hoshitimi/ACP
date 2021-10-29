768de728903dac2252b566f2010d06726654c401
3435 <3435@example.com>
2021-10-29 12:54:15 +0900

    フラグ

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_26_034316) do
+ActiveRecord::Schema.define(version: 2021_10_29_031930) do
 
   create_table "book_reviews", force: :cascade do |t|
     t.integer "comment_id"
