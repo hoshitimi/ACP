1f79fe816399fce315be86c9e1bee6ca17e0c244
3406 <3406@example.com>
2022-01-12 10:46:40 +0900

    シード変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
   User.create(user_id: 88888, user_acount: "先生", authority: 1, point: 120, password: "aa")
   User.create(user_id: 11111, user_acount: "ああ", authority: 0, point: 120, password: "ii")
+  User.create(user_id: 191407, user_acount: "小村真輝", authority: 0, point: 10, password: "look7122")
+  Question.create(user_code: 191407, category: "日常", title: "自分の時間が取れない", contents: "部活や勉強に時間を割いてしまい、自分の時間が取れません。何かいい時間の使い方はありますか？")
+  Question.create(user_code: 191430, category: "勉強", title: "漢字の範囲がわからない", contents: "今度のテストの漢字の範囲がわからない！助けてください！３年 生です")
