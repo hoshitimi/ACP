6abbb7b69fa226f5ade914591a9f2d7ef20a363c
3430 <3430@example.com>
2022-01-23 17:02:18 +0900

    見た目の変更

--- app/controllers/points_controller.rb
+++ app/controllers/points_controller.rb
@@ -3,7 +3,7 @@ class PointsController < ApplicationController
 
   # GET /points or /points.json
   def index
-    @points = Point.all.page(params[:page]).per(20).where(user_id: session[:login_id])
+    @points = Point.all.page(params[:page]).per(10).where(user_id: session[:login_id])
   end
 
   # GET /points/1 or /points/1.json
